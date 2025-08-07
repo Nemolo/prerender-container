e54079603dc6f0478ebf580cd5cc4a3dd52dd606
Fabrizio Marconi <fabrizio.marconi@jef.it>
2025-08-07 23:21:23 +0200

    feat: add whitelist plugin support based on ALLOWED_DOMAINS environment variable

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const server = prerender({
   chromeLocation: '/usr/bin/google-chrome',
   chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--disable-dev-shm-usage'],
 })
+
+// Add whitelist plugin if ALLOWED_DOMAINS environment variable is set
+if (process.env.ALLOWED_DOMAINS) {
+  server.use(prerender.whitelist())
+}
+
 server.use(require('prerender-memory-cache'))
 server.use(prerender.httpHeaders())
 server.start()
